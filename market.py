b8281b743a1f3a1c499d6200baf8dba398d7c90c
FrodeE <frode.ester@gmail.com>
2023-11-29 00:31:39 +0200

    feat: add route for homepage

--- market.py
+++ market.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 
 # Decorator - one step before app is executed.
 @app.route('/') # Our homepage
+@app.route('/home')
 def home_page():
     return render_template('home.html')
 
